fbfbf1f1b5f8c6d3770aa83c187252d9d36e8870
Jibao Mansaray <jibaomansaray@gmail.com>
2022-03-26 06:25:14 +0000

    Split functionalities into functions
    For a hardcoded list of entities we are showing a sub tab that
    will be use for permission and settings

--- projects/CloudStorageHub/src/FileCabinet/SuiteScripts/NsCloudStorageHub/ue_ncsh_attach_ui.js
+++ projects/CloudStorageHub/src/FileCabinet/SuiteScripts/NsCloudStorageHub/ue_ncsh_attach_ui.js
@@ -2,11 +2,19 @@
  * @NApiVersion 2.1
  * @NScriptType UserEventScript
  */
-define(['N/record'],
+define([
+    'N/record',
+    'N/ui/serverWidget',
+    'N/runtime',
+    './libs/entity_helper'
+],
     /**
- * @param{record} record
- */
-    (record) => {
+    * @param { record } recordModule
+    * @param { serverWidget } serverWidgetModule 
+    * @param { runtime } runtimeModule 
+    */
+    (recordModule, serverWidgetModule, runtimeModule, entityHelperModule) => {
+        const URL_PARAM_NAME = 'custscript_cfg_ncsh_url'
         /**
          * Defines the function definition that is executed before record is loaded.
          * @param {Object} scriptContext
@@ -17,6 +25,7 @@ define(['N/record'],
          * @since 2015.2
          */
         const beforeLoad = (scriptContext) => {
+            addUiToForm(scriptContext);
         }
 
         /**
@@ -43,6 +52,86 @@ define(['N/record'],
 
         }
 
-        return {beforeLoad, beforeSubmit, afterSubmit}
+        /**
+         * 
+         * @param {Object} scriptContext
+         */
+        const addUiToForm = (scriptContext) => {
+            if (scriptContext.type !== scriptContext.UserEventType.CREATE) {
+                const script = runtimeModule.getCurrentScript();
+                const TAB_ID = 'custpage_kpama_external_storage_tab';
+                const url = script.getParameter({
+                    name: URL_PARAM_NAME 
+                })
+
+                if (!url) {
+                    return; // Don't shows the external storage tab
+                }
+
+                scriptContext.form.addTab({
+                    id: TAB_ID,
+                    label: 'Cloud Storage'
+                });
+
+                if (entityHelperModule.isEntity(scriptContext.newRecord)) {
+                    addFilesTabFields(addFileSubTab(TAB_ID, scriptContext), scriptContext)
+                    addPermissionTabFields(addPermissionSubTab(TAB_ID, scriptContext), scriptContext);
+                } else {
+                    addFilesTabFields(TAB_ID, scriptContext);
+                }
+            }
+        }
+
+
+        function addFileSubTab(tabId, scriptContext) {
+            const id = 'custpage_kpama_external_storage_tab_files';
+            scriptContext.form.addSubtab({
+                id,
+                tab: tabId,
+                label: 'Files'
+            })
+
+            return id;
+        }
+
+        function addFilesTabFields(containerId, scriptContext) {
+            const script = runtimeModule.getCurrentScript();
+            const url = script.getParameter({
+                name: URL_PARAM_NAME 
+            })
+            const user = runtimeModule.getCurrentUser();
+            const iframeField = scriptContext.form.addField({
+                id:  'custpage_ncsh_files_iframe',
+                type: serverWidgetModule.FieldType.INLINEHTML,
+                label: 'Cloud Files',
+                container: containerId
+            })
+
+            // load the external content
+            iframeField.defaultValue = `<iframe src="${url}/external/front/${user.role}/${user.id}/${scriptContext.newRecord.type}/${scriptContext.newRecord.id}" style="width: 100%; min-height: 600px; border: none; overflow: auto;">
+             </iframe>`;
+        }
+
+        function addPermissionSubTab(tabId, scriptContext) {
+            const id = 'custpage_kpama_external_storage_tab_settings';
+            scriptContext.form.addSubtab({
+                id,
+                tab: tabId,
+                label: 'Permission and settings'
+            });
+            return id
+        }
+
+        function addPermissionTabFields(containerId, scriptContext) {
+            // fake field just for testing
+            scriptContext.form.addField({
+                id: 'custpage_settings',
+                type: serverWidgetModule.FieldType.TEXT,
+                label: 'Fake field',
+                container: containerId 
+            })
+        }
+
+        return { beforeLoad, beforeSubmit, afterSubmit }
 
     });
